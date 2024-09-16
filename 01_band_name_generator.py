614cf1083a368146da8af93de768589a20cac6a1
PatilNikhil1 <nikhilpatil2@outlook.com>
2024-09-16 20:33:16 +0530

    P1

---
+++ 01_band_name_generator.py
@@ -0,0 +1,11 @@
+# greetings
+print("Welcome to the Band Name Generator.")
+
+# insert city
+city = input("What's the name of the city you grew up in?\n")
+
+# insert pet name
+pet = input("What's your pet name?\n")
+
+# final output
+print("Your band name could be " + city + " " + pet)
